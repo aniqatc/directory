f28b419f676e4d933ccbc0a6aa00719dbc067be0
Aniqa <aniqatc97@gmail.com>
2026-09-13 17:30:58 -0400

    Update lastUpdated date to September 13, 2026

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -14,7 +14,7 @@ export const personal: PersonalInfo = {
   title: 'software engineer',
   city: 'ny',
   about: 'a curated archive of my ongoing and completed web design and development projects',
-  lastUpdated: '2026-04-07',
+  lastUpdated: '2026-09-13',
   socialMedia: [
     {
       label: 'github',
